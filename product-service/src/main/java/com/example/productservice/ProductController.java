7a607ebcb59be5de3c62b9b89fcd47b8d8431a64
Maria Eduarda <marinspiraa@gmail.com>
2025-05-29 22:05:57 -0300

    Setting up project

---
+++ product-service/src/main/java/com/example/productservice/ProductController.java
@@ -0,0 +1,19 @@
+package com.example.productservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public List<String> getAll() {
+        return List.of("Laptop", 
+                       "Mouse", 
+                       "Keyboard");
+    }
+}
